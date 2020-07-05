4e9769f1191ab8f54771eb26753c26bca6e0d252
wenzstev <wenzelstev@gmail.com>
2020-07-05 12:38:10 -0400

    Updates

---
+++ precache-manifest.ee2a91b7b8b9b8708accd331a099c52e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ff50e37f0281e5aa7b34add8cd436f",
+    "url": "/writing-sprinter/index.html"
+  },
+  {
+    "revision": "474c512baf1b51cf619f",
+    "url": "/writing-sprinter/static/css/main.27f190fb.chunk.css"
+  },
+  {
+    "revision": "c247a96a974adc828e3e",
+    "url": "/writing-sprinter/static/js/2.81b8afea.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/writing-sprinter/static/js/2.81b8afea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474c512baf1b51cf619f",
+    "url": "/writing-sprinter/static/js/main.638ed181.chunk.js"
+  },
+  {
+    "revision": "c049f01f2b01cdf50a92",
+    "url": "/writing-sprinter/static/js/runtime-main.f38568e9.js"
+  },
+  {
+    "revision": "5e775dbd49e4c6654e3020d662776c71",
+    "url": "/writing-sprinter/static/media/jeans.5e775dbd.jpg"
+  },
+  {
+    "revision": "8166ce752069e7df3a55b993fe9ae1d4",
+    "url": "/writing-sprinter/static/media/leafiron.8166ce75.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/writing-sprinter/static/media/logo.5d5d9eef.svg"
+  }
+]);
